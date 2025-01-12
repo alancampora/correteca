58ce604bad14b85a625f86df1c4d2d2a447d599e
Alan Campora <alan.campora@gmail.com>
2025-01-12 19:35:19 -0300

    changing landing

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import MenRunning from "./icons/men-running";
 
 type Props = {
   onLogin: () => void;
@@ -8,12 +9,12 @@ export default function Landing({ onLogin }: Props) {
   return (
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
+        <MenRunning className="w-40 h-40" />
         <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-          Code like you never did before - move fast, make an impact!
+          Run Smarter, Train Better.
         </h1>
         <p className="text-lg opacity-80 leading-relaxed">
-          Skip the boring setup—this code template lets you build anything you
-          dream of, faster!
+          Unlock your potential with detailed insights and progress tracking.
         </p>
         <Button className="text-sm lg:text-lg p-6" onClick={() => onLogin()}>
           Login
